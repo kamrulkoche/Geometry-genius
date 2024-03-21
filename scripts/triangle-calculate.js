684bebf29ac614d309c16f727e01b491b42f40a3
kamrulkoche <kamrulkoche@gmail.com>
2024-03-22 00:10:37 +0600

    input id add

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,11 @@
 function calculateTriangleArea() {
-  console.log("inside function");
+  //console.log("inside function");
+  const triangleBaseInput = document.getElementById('triangle-base');
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base);
+  const triangleHeightInput = document.getElementById('triangle-height');
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  console.log(height);
 }
